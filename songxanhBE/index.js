07e1f044c57e81bfc01da8d1ba14d647fa64ea06
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-06-25 17:39:47 +0700

    Create Backend to songxanh project
    Create Backend to songxanh project

---
+++ songxanhBE/index.js
@@ -0,0 +1,6 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
