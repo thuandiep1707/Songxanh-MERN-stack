a6768236fb21a3457d6ffc620007493e70efd28c
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-06-27 04:50:35 +0700

    Update main.jsx

--- songxanhFE/src/main.jsx
+++ songxanhFE/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ReactSVG } from 'react-svg';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 
-import svg1 from './assets/svg-icons/home.svg'
+import Header from './components/Header'
+import {routerList} from './routes/router'
+
 
 import './assets/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <div className='text-3xl border-b-2' > Hello world </div>
+    <div>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                {
+                    routerList?.map((item, key) => <Route path={item.path} element={item.element} key={key}/>)
+                }
+            </Routes>
+        </BrowserRouter>
+    </div>
 )
