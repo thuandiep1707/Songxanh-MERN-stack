df1b7b7c90c58e050a9d9e3d3be2a56b29797f0c
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-06-27 04:50:22 +0700

    setup router

---
+++ songxanhFE/src/routes/router.jsx
@@ -0,0 +1,26 @@
+export const routerList = [
+    {
+        path: "/",
+        element: <div>Hello world</div>
+    },
+    {
+        path: "/friend",
+        element: <div>Hello world</div>
+    },
+    {
+        path: "/profile",
+        element: <div>Hello world</div>
+    },
+    {
+        path: "/notification",
+        element: <div>Hello world</div>
+    },
+    {
+        path: "/message",
+        element: <div>Hello world</div>
+    },
+    {
+        path: "/setting",
+        element: <div>Hello world</div>
+    }
+]
