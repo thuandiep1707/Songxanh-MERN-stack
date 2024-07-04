0940dc46567dc56a07e6ed616e495665f8d8460d
Ngọ Văn Long <teranova1707+github@gmail.com>
2024-07-04 11:01:51 +0700

    setup env

--- songxanhBE/index.js
+++ songxanhBE/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+dotenv.config();
+app.get("/", (req, res) => res.send(
+    process.env.DB_CONNECT
+));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(process.env.PORT || 3000, () => {
+    console.log(process.env.DB_CONNECT)
+});
